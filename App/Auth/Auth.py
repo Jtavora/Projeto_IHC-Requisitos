aff14dd60e1f9ac2e52f1656a6a6c97a014324ea
jtavora <jtavora09@gmail.com>
2024-07-04 01:15:00 -0300

    Auth ajustes

--- App/Auth/Auth.py
+++ App/Auth/Auth.py
@@ -1,20 +1,26 @@
+import os
+
+from dotenv import load_dotenv
 from jose import jwt, JWTError
 from App.Models import UserModel, SessionLocal
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
 crypto = CryptContext(schemes=["sha256_crypt"])
+load_dotenv()
+secret_key = os.getenv("SECRET_KEY")
+algorithm = os.getenv("ALGORITHM")
 
 class Auth:
     def __init__(self):
-        self.secret_key = "5c7f45d6f733fc57ea8a3cea62a82531eb93b35987853d93d3d79d32329e4278"
-        self.algorithm = "HS256"
+        self.secret_key = secret_key
+        self.algorithm = algorithm
         self.session = SessionLocal
 
     def user_login(self, data):
         with self.session() as session:
             if data.username == "admin" and data.password == "admin":
-                user = UserModel(username="admin", role="admin", hashed_password=crypto.hash("admin"))
+                user = UserModel(username="admin", hashed_password=crypto.hash("admin"))
             else:
                 user = UserModel.get_user_by_username(session, username=data.username)
                 if not user:
